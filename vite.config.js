9b7afd7ade76119a2e490ceb358d9025d43e1168
21120461 - Dam Hong Hung <damhonghung123@gmail.com>
2024-10-21 09:27:07 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,12 @@ export default defineConfig({
         exclude: [],
     },
     assetsInclude: ["**/*.md"],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+        extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".md"],
+    },
     plugins: [
         {
             name: "load-js-files-as-jsx",
